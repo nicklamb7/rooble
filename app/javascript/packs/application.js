f0f9f557269b6911ce4528c904bde6ff9c70eec2
Nick Lamb <nlamb@hotmail.co.uk>
2021-10-06 16:31:49 +0100

    remove stimulus and ajax search from functions#index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,12 +32,3 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
-
-import "controllers"
-
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
-const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
-application.load(definitionsFromContext(context))
