7440201e21d0db0b4fa3fa9bf9a25682209c7a34
Nick Lamb <nlamb@hotmail.co.uk>
2021-10-17 19:54:59 +0100

    add classes to pages#index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,30 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
+  def index
+    @functions = policy_scope(Function)
+    @types = policy_scope(Type).sort
+
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: 'functions/list', locals: { functions: @functions }, formats: [:html] }
+    end
+
+    if params[:query].present?
+      sql_query = "name ILIKE :query OR description ILIKE :query OR example ILIKE :query"
+      @functions = Function.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @functions = Function.all
+    end
+
+    if params[:query].present?
+      sql_query = "name ILIKE :query OR description ILIKE :query"
+      @types = Type.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @types = Type.all
+    end
+  end
+
   def home
     if params[:query].present?
       sql_query = "name ILIKE :query OR description ILIKE :query OR example ILIKE :query"
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :types
   devise_for :users
   root to: 'pages#home'
+  get '/search', to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
